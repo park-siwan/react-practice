7ee3944f5e4bae8986776ae9671f739bdfbe0c0e
siwan <pswan3168@gmail.com>
2022-01-23 17:35:23 +0900

    index.js에 mocking 시작 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Start the mocking conditionally.
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 ReactDOM.render(
   <>
     <App />
