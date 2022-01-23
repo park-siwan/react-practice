5f9805b16114ecb42f4d31fbd7f64556fbfa7abf
siwan <pswan3168@gmail.com>
2022-01-23 16:54:57 +0900

    msw, typescript, redux 연습 시작

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import ButtonExample from "./components/ButtonExample";
-import { blue, green, red } from "@mui/material/colors";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+// Mui연습
+// import MuiPractice from "./components/MuiPracticec";
+// import { blue, green, red } from "@mui/material/colors";
+// import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import ReduxStudy from "./components/ReduxStudy.tsx";
 function App() {
-  const theme = createTheme({
-    palette: {
-      // primary: {
-      //   main: blue[500],
-      // },
-      secondary: {
-        main: "#d65d00",
-      },
-    },
-    shape: {
-      borderRadius: 24,
-    },
-  });
+  // Mui연습
+  // const theme = createTheme({
+  //   palette: {
+  //     // primary: {
+  //     //   main: blue[500],
+  //     // },
+  //     secondary: {
+  //       main: "#d65d00",
+  //     },
+  //   },
+  //   shape: {
+  //     borderRadius: 24,
+  //   },
+  // });
+  // const theme2 = createTheme({
+  //   palette: {
+  //     primary: {
+  //       main: blue[900],
+  //     },
+  //     secondary: {
+  //       main: "#d65d00",
+  //     },
+  //   },
+  //   shape: {
+  //     borderRadius: 4,
+  //   },
+  // });
   return (
-    <ThemeProvider theme={theme}>
-      <ButtonExample />
-    </ThemeProvider>
+    // // 중첩 테마 공부
+    // <ThemeProvider theme={theme}>
+    //   <MuiPractice />
+    //   <ThemeProvider theme={theme2}>
+    //     <MuiPractice />
+    //   </ThemeProvider>
+    // </ThemeProvider>
+    <ReduxStudy />
   );
 }
 
