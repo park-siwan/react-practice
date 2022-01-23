013c7bec905b962d570d4c02255d193c5e5d1601
siwan <pswan3168@gmail.com>
2022-01-24 01:09:21 +0900

    dipatch 실습

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-
+import { increment, decrement } from "./counterSlice";
 export default function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
   return (
     <div>
       <div>
-        <button>Increment</button>
+        <button onClick={() => dispatch(increment())}>Increment</button>
         <span>{count}</span>
-        <button>Decrement</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
       </div>
     </div>
   );
