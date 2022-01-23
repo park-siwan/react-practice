00d1f690cb33b5a700d3aa8b6d306984d36cae71
siwan <pswan3168@gmail.com>
2022-01-24 00:44:41 +0900

    redux 초기설정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 // Start the mocking conditionally.
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
@@ -9,8 +10,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+  </Provider>,
   document.getElementById("root")
 );
