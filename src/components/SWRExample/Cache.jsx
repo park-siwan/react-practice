21cbf1e35874864124e3316ac9ac50795187c817
siwan <pswan3168@gmail.com>
2022-04-03 01:22:05 +0900

    주석

--- src/components/SWRExample/Cache.jsx
+++ src/components/SWRExample/Cache.jsx
@@ -12,7 +12,7 @@ export default function Cache() {
 
 const Page = () => {
   //SWRConfig로 감싸면 cache에 접근할 수 있다.
-  //mutate는 받은 key로 fetcher를 다시실행한다.
+  //mutate는 받은 key로 fetcher를 다시실행한다. (새로운 값으로 업데이트)
   const { cache, mutate } = useSWRConfig();
   return (
     <div>
